cc711429afb49dba16062f1f04b501a015e6fb0a
Oktavianus Bima Jadiva <93313454+Bimajadivaaa@users.noreply.github.com>
2026-01-18 20:18:18 +0700

    hackerrank solve me first #1
    Implements a simple function to add two numbers and logs the result. Includes example usage and instructions for running the script.

---
+++ solveMeFirst.ts
@@ -0,0 +1,23 @@
+// function main() {
+//     function solveMeFirst(a: number, b:number){
+//         return a + b;
+//     }
+//     const a = parseInt(readLine().trim(),10)
+//     const b = parseInt(readLine().trim(),10)
+
+//     const result = solveMeFirst(a,b)
+//     console.log(result);
+// }
+function solveMeFirst(a:number, b:number){
+    return a + b;
+}
+
+const a = 10;
+const b = 10
+
+const result = solveMeFirst(a,b);
+console.log(result);
+
+
+// Run with
+// npx tsx solveMeFirst.ts
